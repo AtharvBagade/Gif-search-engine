46712196f35404a55dca8842f4561058e5de8d30
AtharvBagade <AtharvBagade@users.noreply.github.com>
2021-10-18 20:34:43 +0530

    Better Code

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import { Header } from './Components/Header';
-import { SearchBar } from './Components/SearchBar';
+import { Giphy } from './Components/Giphy'
 
 function App() {
-
-
-  const [reddit ,setRedditState] =useState({
-
-    currentSubreddit:"",
-    sort:"new",
-    files:[],
-    after:"null",
-    before:"null",
-    page:1
-  });
 
 
 
@@ -23,7 +12,7 @@ function App() {
   return (
     <div className="App">
     <Header />
-    <SearchBar/>
+    <Giphy/>
     </div>
   );
 }
--- src/Components/Giphy.js
+++ src/Components/Giphy.js
@@ -1,37 +1,123 @@
-import React from 'react';
-import  { useState } from "react";
-import { useEffect } from 'react';
+import React, { useState } from 'react'
 
-export const Giphy = (props) => {
+export const Giphy = () => {
 
-    const [state,setState]=useState({
+    const Giphyapi ="yPnwrH8lyHgS1j99J6Mg091Z8OX7VVQ8";
+
+    const [state , setState] = useState({
+
+        query:"",
+        mode:"recent",
+        page:1
+     });
+
+     const [annoyed,setMood] = useState(false)
+
+     const [Giphy,setGiphyState]=useState({
         
         data:[],
         pagination:[],
         meta:[]
     })
 
-    const search_query = "dogs";
+     function Update(type,newdata)
+     {
+       setState(data =>{
+     
+         return{
+           ...data,[type]:newdata
+         }
+   
+       });
+     };
 
-    useEffect(() => {
+     function getStuffGiphy()
+  {
+
+     if(state.query=== "" || typeof(state.query) === "undefined")
+     {
+         setMood(!annoyed);
+     } 
+     
+     else
+    {
+     fetch("https://api.giphy.com/v1/gifs/search?api_key="+ Giphyapi+"&q="+state.query+"&limit=25&offset="+ 0+"&rating=g&lang=en&sort="+state.mode)
+      .then(res => res.json())
+      .then((newdata) =>{
+          setGiphyState(() =>({
+              data:newdata.data,
+              pagination:newdata.pagination,
+              meta:newdata.meta
+          })
+      )})
+      .catch(console.log)
+        }
+  }
+
+    function prev()
+    {
+        fetch("https://api.giphy.com/v1/gifs/search?api_key="+ Giphyapi+"&q="+state.query+"&limit=25&offset="+ (((state.page - 1) * 25) - 1)+"&rating=g&lang=en")
+      .then(res => res.json())
+      .then((newdata) =>{
+          setGiphyState(() =>({
+              data:newdata.data,
+              pagination:newdata.pagination,
+              meta:newdata.meta
+          })
+      )})
+      .catch(console.log)
+      Update('page',state.page-1)
+    }
+
+    function next()
+    {
+        fetch("https://api.giphy.com/v1/gifs/search?api_key="+ Giphyapi+"&q="+state.query+"&limit=25&offset="+ (state.page*25)+"&rating=g&lang=en")
+        .then(res => res.json())
+        .then((newdata) =>{
+            setGiphyState(() =>({
+                data:newdata.data,
+                pagination:newdata.pagination,
+                meta:newdata.meta
+            })
+        )})
+        .catch(console.log)
+        Update('page',state.page+1)
+    }
+
+    return (
+        <div className="Giphy">
+        <div className="Search">
+        <button type="button" onClick={getStuffGiphy} ><i class="fas fa-search"></i></button>
+            <input type="text" id="search_query" name="search_query" placeholder={annoyed ? "Bruh type something first":"Search stuff here"} onChange={
+                (e) => {
+                    Update('query',e.target.value)
+                    getStuffGiphy() }
+                    } />
+        <select className="dropdown1" onChange={
+            (e) =>{ 
+                
+                Update('mode',e.target.value) 
+                getStuffGiphy()        
         
-     let api ="yPnwrH8lyHgS1j99J6Mg091Z8OX7VVQ8";
+        }}>
+                <option value="recent">Recent </option>
+                <option value="relevant">Relevant </option>
+            </select>
+        </div>
+        <div className="data">
+        {
+            Giphy.data && Giphy.data.map(data =><div className="card" key={data.id}><img src={data.images.original_still.url}/>
+        <span id="firstline">From: {data.username}</span> <span id="secondline">Source: <a href={data.bitly_url}>{data.bitly_url}</a></span></div>)
+        
+        }
 
-    fetch(api + ".giphy.com/gifs/search/" + search_query )
-    .then(res => res.json())
-    .then((data) =>{
-        setState(() =>({
-            data:data.data,
-            pagination:data.pagination,
-            meta:data.meta
-        })
-    )})
-    
-    });    
-    
-    
-    return
-    (
-        <div className=""></div>
+       
+        </div>
+        <div className="buttons">
+           { state.page > 1 ? <button onClick={prev}>Prev</button> :"" }
+           {state.page > 1 ? state.page :""}
+           {Giphy.data !== "undefined" || Giphy.data!== null || Giphy.data !=="" ? <button onClick={next}>Next</button> :"" } 
+        </div>
+        </div>
     )
 }
